e7970315fb2934caa75b518ce1f6be12e71d09de
Zechariah <zechariahtan144@gmail.com>
2021-02-20 20:06:04 +0800

    Check type of boolean (true or false)

--- src/Tboolean.ts
+++ src/Tboolean.ts
@@ -5,15 +5,24 @@ import Reporter from "./Reporter"
  */
 export type ITboolean = (item: any) => (reporter: Reporter) => boolean
 /**
- * Checks if next parameter is of type 'boolean'
+ * Checks if next parameter is exactly the same as the boolean passed here
+ * If no parameter passed, checks if next parameter is of type 'boolean'
  */
-export default function Tboolean(): ITboolean {
+export default function Tboolean(bool?: boolean): ITboolean {
 	return item => reporter => {
 		const type_ = typeof item === "boolean"
-		if (!type_)
+		if (!type_) {
 			reporter.complain(
 				`Expected (${reporter.getStack()}) to be of type \`boolean\``
 			)
-		return type_
+			return false
+		}
+		if (bool && item !== bool) {
+			reporter.complain(
+				`Expected (${reporter.getStack()}) to be \`${bool}\``
+			)
+			return false
+		}
+		return true
 	}
 }
