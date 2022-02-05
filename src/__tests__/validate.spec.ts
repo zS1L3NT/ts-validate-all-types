844b104718e8a580b41287bf93c035946834c941
Zechariah <zechariahtan144@gmail.com>
2022-02-06 06:53:17 +0800

    Refactored tests function

--- src/__tests__/validate.spec.ts
+++ src/__tests__/validate.spec.ts
@@ -11,7 +11,7 @@ import {
 	validate
 } from "../index"
 
-const expect_ = (data: any, validator: Validator<any>) => ({
+const _expect = (data: any, validator: Validator<any>) => ({
 	toBe: (errors: number) => {
 		expect(validate(data, validator).errors.length).toBe(errors)
 	}
@@ -19,88 +19,88 @@ const expect_ = (data: any, validator: Validator<any>) => ({
 
 describe("String Validator works when", () => {
 	test("basic string", () => {
-		expect_(123, STRING()).toBe(1)
-		expect_("123", STRING()).toBe(0)
-		expect_([123], STRING()).toBe(1)
-		expect_({ name: "John" }, STRING()).toBe(1)
-		expect_(true, STRING()).toBe(1)
-		expect_(null, STRING()).toBe(1)
-		expect_(undefined, STRING()).toBe(1)
+		_expect(123, STRING()).toBe(1)
+		_expect("123", STRING()).toBe(0)
+		_expect([123], STRING()).toBe(1)
+		_expect({ name: "John" }, STRING()).toBe(1)
+		_expect(true, STRING()).toBe(1)
+		_expect(null, STRING()).toBe(1)
+		_expect(undefined, STRING()).toBe(1)
 	})
 
 	test("regex", () => {
-		expect_("123", STRING(/^123$/)).toBe(0)
-		expect_("", STRING(/^123$/)).toBe(1)
-		expect_(123, STRING(/^123$/)).toBe(1)
+		_expect("123", STRING(/^123$/)).toBe(0)
+		_expect("", STRING(/^123$/)).toBe(1)
+		_expect(123, STRING(/^123$/)).toBe(1)
 	})
 
 	test("list of strings", () => {
-		expect_("abc", STRING("abc", "def")).toBe(0)
-		expect_("abc", STRING("ab", "cd")).toBe(1)
+		_expect("abc", STRING("abc", "def")).toBe(0)
+		_expect("abc", STRING("ab", "cd")).toBe(1)
 	})
 })
 
 describe("Number Validator works when", () => {
 	test("basic number", () => {
-		expect_(123, NUMBER()).toBe(0)
-		expect_("123", NUMBER()).toBe(1)
-		expect_([123], NUMBER()).toBe(1)
-		expect_({ name: "John" }, NUMBER()).toBe(1)
-		expect_(true, NUMBER()).toBe(1)
-		expect_(null, NUMBER()).toBe(1)
-		expect_(undefined, NUMBER()).toBe(1)
+		_expect(123, NUMBER()).toBe(0)
+		_expect("123", NUMBER()).toBe(1)
+		_expect([123], NUMBER()).toBe(1)
+		_expect({ name: "John" }, NUMBER()).toBe(1)
+		_expect(true, NUMBER()).toBe(1)
+		_expect(null, NUMBER()).toBe(1)
+		_expect(undefined, NUMBER()).toBe(1)
 	})
 
 	test("list of numbers", () => {
-		expect_(123, NUMBER(123, 234)).toBe(0)
-		expect_(123, NUMBER(456, 789)).toBe(1)
+		_expect(123, NUMBER(123, 234)).toBe(0)
+		_expect(123, NUMBER(456, 789)).toBe(1)
 	})
 })
 
 describe("Boolean Validator works when", () => {
 	test("basic boolean", () => {
-		expect_(123, BOOLEAN()).toBe(1)
-		expect_("123", BOOLEAN()).toBe(1)
-		expect_([123], BOOLEAN()).toBe(1)
-		expect_({ name: "John" }, BOOLEAN()).toBe(1)
-		expect_(true, BOOLEAN()).toBe(0)
-		expect_(null, BOOLEAN()).toBe(1)
-		expect_(undefined, BOOLEAN()).toBe(1)
+		_expect(123, BOOLEAN()).toBe(1)
+		_expect("123", BOOLEAN()).toBe(1)
+		_expect([123], BOOLEAN()).toBe(1)
+		_expect({ name: "John" }, BOOLEAN()).toBe(1)
+		_expect(true, BOOLEAN()).toBe(0)
+		_expect(null, BOOLEAN()).toBe(1)
+		_expect(undefined, BOOLEAN()).toBe(1)
 	})
 
 	test("true", () => {
-		expect_(true, BOOLEAN(true)).toBe(0)
-		expect_(false, BOOLEAN(true)).toBe(1)
+		_expect(true, BOOLEAN(true)).toBe(0)
+		_expect(false, BOOLEAN(true)).toBe(1)
 	})
 
 	test("false", () => {
-		expect_(true, BOOLEAN(false)).toBe(1)
-		expect_(false, BOOLEAN(false)).toBe(0)
+		_expect(true, BOOLEAN(false)).toBe(1)
+		_expect(false, BOOLEAN(false)).toBe(0)
 	})
 })
 
 describe("List Validator works when", () => {
 	test("basic list validator", () => {
-		expect_(123, LIST()).toBe(1)
-		expect_("123", LIST()).toBe(1)
-		expect_([123], LIST()).toBe(0)
-		expect_({ name: "John" }, LIST()).toBe(1)
-		expect_(true, LIST()).toBe(1)
-		expect_(null, LIST()).toBe(1)
-		expect_(undefined, LIST()).toBe(1)
+		_expect(123, LIST()).toBe(1)
+		_expect("123", LIST()).toBe(1)
+		_expect([123], LIST()).toBe(0)
+		_expect({ name: "John" }, LIST()).toBe(1)
+		_expect(true, LIST()).toBe(1)
+		_expect(null, LIST()).toBe(1)
+		_expect(undefined, LIST()).toBe(1)
 	})
 
 	test("list of validators", () => {
-		expect_([], LIST(STRING())).toBe(0)
-		expect_([], LIST(NUMBER())).toBe(0)
-		expect_([123], LIST(STRING())).toBe(1)
-		expect_([123], LIST(STRING(), NUMBER())).toBe(0)
-		expect_([123], LIST(NUMBER(234))).toBe(1)
-		expect_([123], LIST(OR(NUMBER(234), NUMBER(123))))
-		expect_(["123"], LIST(STRING(), NUMBER(123))).toBe(0)
-		expect_(["123"], LIST(STRING("234"))).toBe(1)
-		expect_([{ name: "" }], LIST(OBJECT({ name: STRING() }))).toBe(0)
-		expect_(
+		_expect([], LIST(STRING())).toBe(0)
+		_expect([], LIST(NUMBER())).toBe(0)
+		_expect([123], LIST(STRING())).toBe(1)
+		_expect([123], LIST(STRING(), NUMBER())).toBe(0)
+		_expect([123], LIST(NUMBER(234))).toBe(1)
+		_expect([123], LIST(OR(NUMBER(234), NUMBER(123))))
+		_expect(["123"], LIST(STRING(), NUMBER(123))).toBe(0)
+		_expect(["123"], LIST(STRING("234"))).toBe(1)
+		_expect([{ name: "" }], LIST(OBJECT({ name: STRING() }))).toBe(0)
+		_expect(
 			[{ name: "" }, { age: 1 }],
 			LIST(OBJECT({ name: STRING() }))
 		).toBe(2)
@@ -109,45 +109,45 @@ describe("List Validator works when", () => {
 
 describe("Object Validator works when", () => {
 	test("basic object", () => {
-		expect_(123, OBJECT()).toBe(1)
-		expect_("123", OBJECT()).toBe(1)
-		expect_([123], OBJECT()).toBe(1)
-		expect_({ name: "John" }, OBJECT()).toBe(0)
-		expect_(true, OBJECT()).toBe(1)
-		expect_(null, OBJECT()).toBe(1)
-		expect_(undefined, OBJECT()).toBe(1)
+		_expect(123, OBJECT()).toBe(1)
+		_expect("123", OBJECT()).toBe(1)
+		_expect([123], OBJECT()).toBe(1)
+		_expect({ name: "John" }, OBJECT()).toBe(0)
+		_expect(true, OBJECT()).toBe(1)
+		_expect(null, OBJECT()).toBe(1)
+		_expect(undefined, OBJECT()).toBe(1)
 	})
 
 	test("too many properties", () => {
-		expect_({ name: "John" }, OBJECT({})).toBe(1)
-		expect_({ name: "John", age: 1 }, OBJECT({ name: STRING() })).toBe(1)
+		_expect({ name: "John" }, OBJECT({})).toBe(1)
+		_expect({ name: "John", age: 1 }, OBJECT({ name: STRING() })).toBe(1)
 	})
 
 	test("too few properties", () => {
-		expect_({}, OBJECT({ name: STRING() })).toBe(1)
-		expect_(
+		_expect({}, OBJECT({ name: STRING() })).toBe(1)
+		_expect(
 			{ name: "John" },
 			OBJECT({ name: STRING(), age: NUMBER() })
 		).toBe(1)
-		expect_({}, OBJECT({ name: STRING(), age: NUMBER() })).toBe(2)
+		_expect({}, OBJECT({ name: STRING(), age: NUMBER() })).toBe(2)
 	})
 
 	test("different properties", () => {
-		expect_({ name: "John" }, OBJECT({ name: STRING() })).toBe(0)
-		expect_({ name: "John" }, OBJECT({ age: NUMBER() })).toBe(2)
-		expect_(
+		_expect({ name: "John" }, OBJECT({ name: STRING() })).toBe(0)
+		_expect({ name: "John" }, OBJECT({ age: NUMBER() })).toBe(2)
+		_expect(
 			{ name: "John", age: "1" },
 			OBJECT({ name: STRING(), age: NUMBER() })
 		).toBe(1)
-		expect_(
+		_expect(
 			{ names: ["John"], settings: {} },
 			OBJECT({ names: LIST(STRING()), settings: LIST() })
 		).toBe(1)
-		expect_(
+		_expect(
 			{ names: ["John"], settings: {} },
 			OBJECT({ names: LIST(STRING()), settings: OBJECT({}) })
 		).toBe(0)
-		expect_(
+		_expect(
 			{ names: ["John"], settings: {} },
 			OBJECT({ names: LIST(STRING()), settings: OBJECT() })
 		).toBe(0)
@@ -156,32 +156,32 @@ describe("Object Validator works when", () => {
 
 describe("Null Validator", () => {
 	test("basic null", () => {
-		expect_(123, NULL()).toBe(1)
-		expect_("123", NULL()).toBe(1)
-		expect_([123], NULL()).toBe(1)
-		expect_({ name: "John" }, NULL()).toBe(1)
-		expect_(true, NULL()).toBe(1)
-		expect_(null, NULL()).toBe(0)
-		expect_(undefined, NULL()).toBe(1)
+		_expect(123, NULL()).toBe(1)
+		_expect("123", NULL()).toBe(1)
+		_expect([123], NULL()).toBe(1)
+		_expect({ name: "John" }, NULL()).toBe(1)
+		_expect(true, NULL()).toBe(1)
+		_expect(null, NULL()).toBe(0)
+		_expect(undefined, NULL()).toBe(1)
 	})
 })
 
 describe("Undefined Validator", () => {
 	test("basic undefined", () => {
-		expect_(123, UNDEFINED()).toBe(1)
-		expect_("123", UNDEFINED()).toBe(1)
-		expect_([123], UNDEFINED()).toBe(1)
-		expect_({ name: "John" }, UNDEFINED()).toBe(1)
-		expect_(true, UNDEFINED()).toBe(1)
-		expect_(null, UNDEFINED()).toBe(1)
-		expect_(undefined, UNDEFINED()).toBe(0)
+		_expect(123, UNDEFINED()).toBe(1)
+		_expect("123", UNDEFINED()).toBe(1)
+		_expect([123], UNDEFINED()).toBe(1)
+		_expect({ name: "John" }, UNDEFINED()).toBe(1)
+		_expect(true, UNDEFINED()).toBe(1)
+		_expect(null, UNDEFINED()).toBe(1)
+		_expect(undefined, UNDEFINED()).toBe(0)
 	})
 })
 
 describe("Or Validator", () => {
 	test("basic or", () => {
-		expect_(0, OR(STRING(), NUMBER())).toBe(0)
-		expect_(0, OR(NULL(), UNDEFINED())).toBe(1)
-		expect_(0, OR(STRING("hi", "bye"), NUMBER(1, 2))).toBe(1)
+		_expect(0, OR(STRING(), NUMBER())).toBe(0)
+		_expect(0, OR(NULL(), UNDEFINED())).toBe(1)
+		_expect(0, OR(STRING("hi", "bye"), NUMBER(1, 2))).toBe(1)
 	})
 })
