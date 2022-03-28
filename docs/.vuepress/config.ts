06d4f27f6f7dfa5aa13027704b911d5e26318c1f
Zechariah Tan <zechariahtan144@gmail.com>
2022-03-28 18:29:37 +0800

    Got vuepress to work

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vuepress/config"
+
+export default defineConfig({
+	title: "Validate Any",
+	description: "Type validation for JavaScript at runtime",
+	themeConfig: {
+		repo: "zS1L3NT/ts-npm-validate-any",
+		searchPlaceholder: "Search",
+		smoothScroll: true,
+		nav: [
+			{ text: "Home", link: "/" },
+			{ text: "Guides", link: "/guides/Introduction.html" },
+			{ text: "References", link: "/references/" }
+		],
+		sidebar: [
+			{
+				title: "Guide",
+				children: ["/guides/Introduction"]
+			}
+		]
+	}
+})
---
+++ docs/README.md
@@ -0,0 +1,12 @@
+---
+home: true
+actionText: Get Started
+actionLink: /guides/Introduction.html
+features:
+  - title: TypeScript Annotations
+    details: TypeScript annotations are generated based on schemas you write.
+  - title: Runtime Validation
+    details: Does validation of data at runtime so your data types are correct before you use it.
+  - title: Works with Express/Next
+    details: Provides validation middlewares that you can use with your Express/Next server.
+---
